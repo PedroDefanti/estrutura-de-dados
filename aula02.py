e2f8dcba99cb16c55ca88271616a303efc6888da
PedroDefanti <168847376+PedroDefanti@users.noreply.github.com>
2025-09-16 21:38:12 -0300

    aula2 de estrutura

---
+++ aula02.py
@@ -0,0 +1,16 @@
+# import numpy as ny
+# print('Arranjo criado a partir de um intervalo:')
+# a=pow(2,2)
+# # z=ny.arange(0,2,.2)
+# # print(z)
+# # y=(5*z)
+# # w=y+z
+# # print(w)
+# print(a)
+
+# print('Arranjo de 2x3:')
+# y=ny.arange(0,3,.2)
+
+# z=(5*y)
+# z.shape=3,5
+# print(z[1])
